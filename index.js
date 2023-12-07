b5f23868d654d06241de23051834ac6f3c092c21
Dev-Gosling <72206607+Dev-Gosling@users.noreply.github.com>
2023-12-07 16:56:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+function searchFine() {
+    let searchFor = document.getElementById("searchbar_input").value.toLocaleLowerCase()
+    
+    let fines = document.querySelectorAll(".fine");
+    for (var i = 0; i < fines.length; i++) {
+        if (fines[i].querySelectorAll(".fineText")[0].innerHTML.toLocaleLowerCase().includes(searchFor)) {
+            fines[i].classList.add("showing")
+            fines[i].classList.remove("hiding")
+        } else {
+            fines[i].classList.remove("showing")
+            fines[i].classList.add("hiding")
+        }
+        
+    }
+}
+
+function selectFine(event) {
+    let element = event.target
+    console.log(element.tagName);
+    if (element.tagName == "TD") element = element.parentElement
+    if (element.tagName == "I") element = element.parentElement.parentElement
+
+    if (element.classList.contains("selected")) {
+        element.classList.remove("selected")
+    } else {
+        element.classList.add("selected")
+    }
+
+    startCalculating()
+}
+
+function startCalculating() {
+    let fineResult = document.getElementById("fineResult")
+    let fineAmount = 0
+
+    let wantedResult = document.getElementById("wantedsResult")
+    let wantedAmount = 0
+
+    let reasonResult = document.getElementById("reasonResult")
+    let reasonText = ""
+    let plate = document.getElementById("plateInput_input").value
+    let systemwanteds = document.getElementById("systemwantedsInput_input").value
+
+    let infoResult = document.getElementById("infoResult")
+    let noticeText = ""
+    let removeWeaponLicense = false
+    let removeDriverLicense = false
+
+    let fineCollection = document.querySelectorAll(".selected")
+    for (var i = 0; i < fineCollection.length; i++) {
+        fineAmount = fineAmount + parseInt(fineCollection[i].querySelector(".fineAmount").getAttribute("data-fineamount"))
+
+        wantedAmount = wantedAmount + parseInt(fineCollection[i].querySelector(".wantedAmount").getAttribute("data-wantedamount"))
+        if (wantedAmount > 5) wantedAmount = 5
+        
+
+        let now = new Date();
+        let hour = now.getHours();
+        if (hour < 10) hour = "0" + hour
+
+        let minute = now.getMinutes();
+        if (minute < 10) minute = "0" + minute
+
+        let day = now.getDate()
+        if (day < 10) day = "0" + day
+
+        let month = now.getMonth() + 1
+        if (month < 10) month = "0" + month
+
+        let fineText = ""
+        if (fineCollection[i].querySelector(".fineText").innerHTML.includes("<i>")) {
+            fineText = fineCollection[i].querySelector(".fineText").innerHTML.split("<i>")[0]
+        } else {
+            fineText = fineCollection[i].querySelector(".fineText").innerHTML
+        }
+
+        if (reasonText == "") {
+            reasonText = `${day}.${month} ${hour}:${minute} - ${fineCollection[i].querySelector(".paragraph").innerHTML} - ${fineText}`
+        } else {
+            reasonText += ` + ${day}.${month} ${hour}:${minute} - ${fineCollection[i].querySelector(".paragraph").innerHTML} - ${fineText}`
+        }
+
+        if (fineCollection[i].getAttribute("data-removedriverlicence") == "true") removeDriverLicense = true
+        if (fineCollection[i].getAttribute("data-removeweaponlicence") == "true") removeWeaponLicense = true
+    }
+
+    if (plate != "") {
+        reasonText += ` + ${plate.toLocaleUpperCase()}`
+    }
+
+    if (systemwanteds != "") {
+        reasonText += ` + ${systemwanteds} Systemwanteds`
+    }
+
+
+    if (removeDriverLicense) {
+        noticeText = "Führerschein entziehen"
+    }
+    if (removeWeaponLicense) {
+        if (noticeText =="") {
+            noticeText = "Waffenschein entziehen"
+        } else {
+            noticeText = noticeText + " + Waffenschein entziehen"
+        }
+    }
+
+
+    infoResult.innerHTML = `<b>Information:</b> ${noticeText}`
+    fineResult.innerHTML = `<b>Geldstrafe:</b> <font style="user-select: all;">$${fineAmount}</font>`
+    wantedResult.innerHTML = `<b>Wanteds:</b> <font style="user-select: all;">${wantedAmount}</font>`
+    reasonResult.innerHTML = `<b>Grund:</b> <font style="user-select: all;">${reasonText}</font>`
+}
